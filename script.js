3ff345af46c301618991776ab2ad2ce0f67aa930
Brianna Hawkins <155006608+bhaw16@users.noreply.github.com>
2025-02-13 15:33:00 -0500

    fixed issue where Movies don't show in table

--- script.js
+++ script.js
@@ -16,20 +16,23 @@ function displayMovieInTable(movie) {
         throw new TypeError("Parameter must be of type Movie.");
     }
     var movieRow = document.createElement("tr");
-    movieRow.class = "movie-rows";
+    movieRow.className = "movie-rows";
     for (var i = 0; i < 5; i++) {
         var tableData = document.createElement("td");
         console.log(`movie-${movies.indexOf(movie)}`);
-        tableData.class = `movie-${movies.indexOf(movie)}`;
+        tableData.className = `movie-${movies.indexOf(movie)}`;
         var movieAttributeData = getMovieAttribute(movie, i);
         console.log(movieAttributeData);
+        console.log(tableData);
         (movieAttributeData instanceof HTMLAnchorElement) ? tableData.insertAdjacentElement("afterbegin", movieAttributeData) : tableData.insertAdjacentText("afterbegin", movieAttributeData);
+        movieRow.insertAdjacentElement("beforeend", tableData);
     }
-    var movieRowData = document.getElementsByClassName(`movie-${movies.indexOf(movie)}`);
+    /*var movieRowData = document.getElementsByClassName(`movie-${movies.indexOf(movie)}`);
+    console.log(document.getElementsByClassName(`movie-${movies.indexOf(movie)}`));
     console.log(movieRowData);
     for (var i = 0; i < movieRowData.length; i++) {
         movieRow.insertAdjacentElement("beforeend", movieRowData[i]);
-    }
+    }*/
     document.getElementsByTagName("tbody")[0].insertAdjacentElement("beforeend", movieRow);
 }
 
